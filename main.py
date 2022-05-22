3050a175fff45af0ce43bb15f7ef63b23a9b7a71
Владислав <vladyak99@gmail.com>
2022-05-22 18:25:33 +0400

    двухэтапный парсер

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ import pandas as pd
 from firstPart import df1
 from secondPart import df2
 
+import numpy as np
 import matplotlib.pyplot as plt
 
-#слияние датасетов в выгрузка в файл
+#объединение фреймов данных, вывод в csv
 OUTFILE = 'stats.csv'
 dfа = pd.merge(df1, df2, how='outer')
 dfа.to_csv(OUTFILE)
 
 fig = plt.figure()
-# Добавление на рисунок прямоугольной (по умолчанию) области рисования
-fig.add_axes([0, 0, 1, 1])
-plt.scatter(1.0, 1.0)
-plt.show
+plt.scatter(1.0, 1.0) 
+plt.show()
