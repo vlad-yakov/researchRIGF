a3c0ecad574a7ada668b463dc0bbf74ffb98e70c
Владислав <vladyak99@gmail.com>
2022-05-22 17:44:08 +0400

    двухэтапный парсер

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import pandas as pd
 from firstPart import df1
 from secondPart import df2
 
+import matplotlib.pyplot as plt
+
+#слияние датасетов в выгрузка в файл
 OUTFILE = 'stats.csv'
-
 dfа = pd.merge(df1, df2, how='outer')
-
 dfа.to_csv(OUTFILE)
+
+
