229949ba8f2364bbbebf7cb10f04e2aec214d124
Владислав <vladyak99@gmail.com>
2022-05-22 18:50:11 +0400

    двухэтапный парсер

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import pandas as pd
-from firstPart import df1
-from secondPart import df2
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-#объединение фреймов данных, вывод в csv
-OUTFILE = 'stats.csv'
-dfа = pd.merge(df1, df2, how='outer')
-dfа.to_csv(OUTFILE)
+from firstPart import df1
+from secondPart import df2
 
+# объединение фреймов данных, вывод в csv
+OUTFILE = 'stats.csv'
+df = pd.merge(df1, df2, how='outer')
+df.to_csv(OUTFILE)
+
+plt.interactive(True)
 fig = plt.figure()
-plt.scatter(1.0, 1.0) 
+plt.scatter(1.0, 1.0)
+
 plt.show()
+
+# разобраться, какого хрена не запускается
